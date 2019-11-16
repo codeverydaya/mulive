1e8600461b927ae5964490a2709823a688d82e7e
lmy-hub <1013127768@qq.com>
2019-11-17 00:47:13 +0800

    init

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+console.disableYellowBox = true;
 
 import AppContainer from "./rnsrc/navigation/appcontainer";
 AppRegistry.registerComponent(appName, () => AppContainer);
